78864b0b0466c65c643c0374b52e041a08c962e9
Rohan Kalra <ronziireturns@gmail.com>
2013-03-04 22:07:26 +0530

    Jade template used

--- app.js
+++ app.js
@@ -15,16 +15,21 @@
 
  app.configure(function(){
  	app.set('port', process.env.PORT || 3000);
+ 	app.set('views', __dirname + '/views');
+ 	app.set('view engine', 'jade');
  	app.use(express.favicon());
  	app.use(express.logger('dev'));
  	app.use(express.bodyParser());
+ 	app.use(express.cookieParser());
+ 	app.use(express.session({secret : 'faad'}));
  	app.use(express.methodOverride());
+ 	app.use(app.router);
  	app.use(express.static(path.join(__dirname, 'public')));
  });
 
  app.configure('development', function(){
-  	app.use(express.errorHandler());
-});
+ 	app.use(express.errorHandler());
+ });
 
  var server = http.createServer(app)
  , io = io.listen(server);
@@ -33,6 +38,9 @@
 
  server.listen(app.get('port'), function(){
  	console.log("Express server listening on port " + app.get('port'));
+ });
+ app.get('/', function(req, res){
+  res.render('index');
  });
 
  io.sockets.on('connection',function(socket){
---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  // UI Logic
+  function getContentById(id){
+    var path = 'content/' + id + '/index.html';
+    $.get(path, function(data){
+      $('#mainBody').html(data);    
+    });
+  }
+  $('.nav li a').click(function(){
+    getContentById(this.id);
+  })
+  getContentById('login');
+
+  var socket = io.connect('http://localhost:3000');
+  socket.on('chat', function (data) {
+    console.log(data);
+    addMessageContainer(data);
+  });
+  socket.on('userlist', function(list){
+  $('#onlineUsers').html('');
+    for(var i=0;i<list.length;i++){
+      $('#onlineUsers').append('<li><a href="#">' + list[i].username + '</a></li>');
+    }
+  });
+
+})
